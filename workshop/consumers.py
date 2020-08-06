ff82be508f23fd31b64488956bc83c0793fb3098
ab.reza <a.re.morteza@gmail.com>
2020-08-06 22:29:09 +0430

    fix websocket handlers

--- workshop/consumers.py
+++ workshop/consumers.py
@@ -25,22 +25,38 @@ class BoardConsumer(AsyncJsonWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['data']
+        message = text_data_json.get('data')
+        message_type = text_data_json.get('type')
 
         # Send message to room
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'board_message',
+                'type': message_type,
                 'data': message
             }
         )
 
     # Receive message from room
-    async def board_message(self, event):
-        message = event['data']
+    async def PASS_DRAWING_STATE(self, event):
+        print(event)
+        message = event.get('data')
+        message_type = event.get('type')
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'data': message
+            'data': message,
+            'type': message_type
+        }))
+
+    # Receive message from room
+    async def JOIN_TO_GROUP_ROOM(self, event):
+        print(event)
+        message = event.get('data')
+        message_type = event.get('type')
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'data': message,
+            'type': message_type
         }))
