767ac1341b485d6a947d43d73c454b8e68eb73eb
erfanmotnt <moeini.erfan@gmail.com>
2020-08-17 14:55:42 +0430

    add auction model

--- auction/models.py
+++ auction/models.py
@@ -8,15 +8,26 @@ class Account(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, unique=True, related_name='account')
 
+class OneTimeAuction(models.Model):
+    winner =  models.ForeignKey(Account, null=True, on_delete=models.CASCADE)
+    max_bid = models.IntegerField(default=0)
+
 
 class OneTimeBidder(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='oneTimeBidder')
+    auction = models.ForeignKey(OneTimeAuction, on_delete=models.CASCADE, related_name='bidders')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='oneTimeBidders')
     value = models.IntegerField()
     bid = models.IntegerField(default=0)
     
  
+class BritishAuction(models.Model):
+    winner =  models.ForeignKey(Account, null=True, on_delete=models.CASCADE)
+    max_bid = models.IntegerField(default=0)
+    Increaseـrate = models.IntegerField(default=0)
+
 class BritishBidder(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='britishBidder')
+    auction = models.ForeignKey(BritishAuction, on_delete=models.CASCADE, related_name='bidders')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='britishBidders')
     value = models.IntegerField()
     bid = models.IntegerField(default=0)
     
