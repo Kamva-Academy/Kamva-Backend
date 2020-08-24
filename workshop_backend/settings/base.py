f132d22188bfa2a1b1fdebe213a041a182c0d56d
Erfan Loghmani <Elkiscoming@gmail.com>
2020-08-24 09:38:05 +0430

    Change static_url

--- workshop_backend/settings/base.py
+++ workshop_backend/settings/base.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/djangostatic/'
+STATIC_URL = '/api/static/'
 
 MEDIA_URL = '/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
