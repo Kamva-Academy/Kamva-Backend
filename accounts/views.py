34a416e8cf573166825b475d0764f0986dabe846
ab.reza <a.re.morteza@gmail.com>
2020-08-24 09:34:52 +0430

    fix login_required in UploadAnswerView

--- accounts/views.py
+++ accounts/views.py
@@ -247,6 +247,7 @@ class ChangePass(APIView):
 class UploadAnswerView(APIView):
     parser_class = (FileUploadParser,)
 
+    @login_required
     def post(self, request):
         if 'file' not in request.data:
             raise ParseError("Empty content")
