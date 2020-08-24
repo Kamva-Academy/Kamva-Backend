6dae4e103bcdb08b7231165c0333a5183b879376
ab.reza <a.re.morteza@gmail.com>
2020-08-24 09:20:48 +0430

    add login_required to UploadAnswerView

--- accounts/views.py
+++ accounts/views.py
@@ -244,6 +244,7 @@ class ChangePass(APIView):
         return Response({'success': True},status=status.HTTP_200_OK)
 
 
+@login_required
 class UploadAnswerView(APIView):
     parser_class = (FileUploadParser,)
 
