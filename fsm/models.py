c92c7a90f497c5e00a38e397ceef604856470e6a
Erfan <moeini.erfan@gmail.com>
2020-08-29 20:38:06 +0430

    add history to models

--- fsm/models.py
+++ fsm/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from model_utils.managers import InheritanceManager
-
+from accounts.models import *
 
 class FSM(models.Model):
     name = models.CharField(max_length=100)
@@ -28,7 +28,7 @@ class Ability(models.Model):
     edge = models.ForeignKey(FSMEdge, null=True, on_delete=models.CASCADE, related_name='abilities')
     name = models.CharField(max_length=150)
     value = models.BooleanField()
-
+    team_history = models.ForeignKey('TeamHistory', null=True, on_delete=models.CASCADE, related_name='abilities')
     def __str__(self):
         return self.name
 
@@ -61,6 +61,7 @@ class Game(Widget):
 
 
 class Answer(models.Model):
+    submited_answer = models.OneToOneField('SubmitedAnswer', null=True, on_delete=models.CASCADE, unique=True, related_name='%(class)s')
     class Meta:
         abstract = True
 
@@ -106,3 +107,16 @@ class ProblemMultiChoice(Problem):
 class Choice(models.Model):
     problem = models.ForeignKey(ProblemMultiChoice, null=True, on_delete=models.CASCADE, related_name='choices')
     text = models.TextField()
+
+class SubmitedAnswer(models.Model):
+    participant = models.ForeignKey(Participant, null=True, on_delete=models.CASCADE, related_name='submited_answers')
+    publish_date = models.DateTimeField(null=True, blank=True)
+    team_history = models.ForeignKey('TeamHistory', null=True, on_delete=models.CASCADE, related_name='answers')
+
+class TeamHistory(models.Model):
+    team = models.ForeignKey(Team, null=True, on_delete=models.CASCADE, related_name='histories')
+    state = models.ForeignKey(Participant, null=True, on_delete=models.CASCADE, related_name='histories')
+    grade = models.IntegerField(default=0)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    
