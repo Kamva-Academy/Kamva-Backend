811f836a81f95e6f5607a8b91c789b2b724bc16d
ce441-981-94105836 <f.labbaf98@gmail.com>
2020-08-31 15:59:22 +0430

    TestMember permission

--- fsm/views/permissions.py
+++ fsm/views/permissions.py
@@ -7,3 +7,14 @@ class MentorPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         print(request.user)
         return bool(request.user and request.user.is_authenticated and request.user.is_mentor)
+
+
+class TestMembersOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        user = request.user
+        username = user.username
+        print(request.user)
+        if username.startswith("TEST_"):
+            return True
+        return False
