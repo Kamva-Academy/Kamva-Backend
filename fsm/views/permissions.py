33a2460062f133e05042410313665152567b222e
ce441-981-94105836 <f.labbaf98@gmail.com>
2020-08-31 09:55:44 +0430

    permission

--- fsm/views/permissions.py
+++ fsm/views/permissions.py
@@ -6,4 +6,4 @@ class MentorPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         print(request.user)
-        return request.user.is_mentor
+        return bool(request.user and request.user.is_authenticated and request.user.is_mentor)
