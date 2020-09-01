adff4a53051a539a527a9fa038033dc56074350a
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2020-09-01 10:27:49 +0430

    remove prints function from code

--- auction/views/permissions.py
+++ auction/views/permissions.py
@@ -1,9 +1,11 @@
 from rest_framework import permissions
 from accounts.models import Member, Team, Participant
+import logging
+logger = logging.getLogger(__name__)
 
 
 class MentorPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        print(request.user)
+        logger.debug(f'user {request.user} has permission')
         return request.user.is_mentor
