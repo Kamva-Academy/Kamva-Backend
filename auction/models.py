186d5eeb1f349ecf3f7836594641c1a2a88f938b
ce441-981-94105836 <f.labbaf98@gmail.com>
2020-09-02 10:31:40 +0430

    debug Auction game change timeZone

--- auction/models.py
+++ auction/models.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import *
+
 class OneTimeAuction(models.Model):
     auction_pay_type = models.IntegerField(default=1)
     winner =  models.ForeignKey('OneTimeBidder', null=True, on_delete=models.CASCADE)
-    start_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    end_time = models.DateTimeField(null=True, blank=True, default=datetime.now() + timedelta(minutes=3))
-
+    start_time = models.DateTimeField(default=datetime.now() + timedelta(days=1), null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True, default=datetime.now() + timedelta(days=1) +timedelta(minutes=3))
 
 class OneTimeBidder(models.Model):
     auction = models.ForeignKey(OneTimeAuction, on_delete=models.CASCADE, related_name='bidders')
