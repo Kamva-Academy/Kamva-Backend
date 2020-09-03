4a7e95f2ce61fa06d1d6dead43fe38cb3fe4970b
ce441-981-94105836 <f.labbaf98@gmail.com>
2020-09-03 13:02:54 +0430

    fix error 500 when fsmState set to None

--- fsm/views/functions.py
+++ fsm/views/functions.py
@@ -5,6 +5,6 @@ def team_change_current_state(team, state):
     #fsm change checked
     #create history
     team.current_state = state
-    if len(TeamHistory.objects.filter(team=team.id, state=state.id)) == 0:
+    if state and len(TeamHistory.objects.filter(team=team.id, state=state.id)) == 0:
         history = TeamHistory.objects.create(start_time=timezone.localtime(), team=team, state=state)
     team.save()
