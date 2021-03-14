9c53cbbdf67b919a08c4a89334ee4760dbf4322b
f.labbaf98 <f.labbaf98@gmail.com>
2021-03-14 19:32:45 +0330

    participant admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -27,19 +27,49 @@ import csv
 from django.http import HttpResponse
 
 
+
 class ParticipantResource(resources.ModelResource):
     name = Field()
     email = Field()
-    email_verified = Field()
+    phone = Field()
+    username = Field()
+    answer = Field()
+    doc = Field()
+    gender = Field()
+    school = Field()
+    city = Field()
+    team_count = Field()
+    team = Field()
+
 
     class Meta:
         model = Participant
         fields = ['gender', 'grade', 'city', 'school', 'phone_number', 'document', 'phone_number', 'team',
-                  'phone_number', 'ent_answer', 'is_activated', 'accepted']
+                  'phone_number', 'ent_answer', 'is_activated', 'team_count']
 
     def dehydrate_name(self, participant):
         try:
             return participant.member.first_name
+        except Exception:
+            return ''
+
+    def dehydrate_username(self, participant):
+        try:
+            return participant.member.username
+        except Exception:
+            return ''
+
+    def dehydrate_team(self, participant):
+        try:
+            return participant.event_team
+        except Exception:
+            return ''
+
+    def dehydrate_team_count(self, participant):
+        print("helooooo")
+        try:
+            t = participant.event_team
+            return len(participant.event_team.team_participants.all())
         except Exception:
             return ''
 
@@ -49,39 +79,51 @@ class ParticipantResource(resources.ModelResource):
         except Exception:
             return ''
 
-    def dehydrate_email_verified(self, participant):
+    def dehydrate_gender(self, participant):
         try:
-            return participant.member.is_active
+            return participant.member.gender
         except Exception:
-            return False
+            return ''
 
-    def dehydrate_ent_answer(self, participant):
+    def dehydrate_grade(self, participant):
         try:
-            if participant.ent_answer:
-                return "rastaiha.ir/api/media/" + str(participant.ent_answer)
+            return participant.member.grade
+        except Exception:
+            return ''
+
+    def dehydrate_school(self, participant):
+        try:
+            return participant.member.school
+        except Exception:
+            return ''
+
+    def dehydrate_city(self, participant):
+        try:
+            return participant.member.city
+        except Exception:
+            return ''
+
+    def dehydrate_phone(self, participant):
+        try:
+            return participant.member.phone_number
+        except Exception:
+            return ''
+
+    def dehydrate_answer(self, participant):
+        try:
+            if participant.selection_doc:
+                return "workshop.rastaiha.ir/api/media/" + str(participant.selection_doc)
             else:
                 return ''
         except Exception:
             return ''
 
-    def dehydrate_document(self, participant):
+    def dehydrate_doc(self, participant):
         try:
-            return "rastaiha.ir/api/media/" + str(participant.document)
-        except Exception:
-            return ''
-
-    def dehydrate_team(self, participant):
-        try:
-            if participant.team:
-                team = participant.team
-                team_name = str(team.id) + " ("
-                for p in team.team_participants.all():
-                    team_name += str(p.member.first_name) + ", "
-                team_name += ")"
-                return team_name
+            if participant.member.document:
+                return "workshop.rastaiha.ir/api/media/" + str(participant.member.document)
             else:
                 return ''
-
         except Exception:
             return ''
 
@@ -113,23 +155,23 @@ class IsPaidFilter(admin.SimpleListFilter):
         return queryset
 
 
-class IsEmailVerifiedFilter(admin.SimpleListFilter):
-    title = 'is_email_verified'
-    parameter_name = 'is_email_verify'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('Yes', 'Yes'),
-            ('No', 'No'),
-        )
-
-    def queryset(self, request, queryset):
-        value = self.value()
-        if value == 'Yes':
-            return queryset.filter(member__is_active=True)
-        elif value == 'No':
-            return queryset.exclude(member__is_active=True)
-        return queryset
+# class IsEmailVerifiedFilter(admin.SimpleListFilter):
+#     title = 'is_email_verified'
+#     parameter_name = 'is_email_verify'
+#
+#     def lookups(self, request, model_admin):
+#         return (
+#             ('Yes', 'Yes'),
+#             ('No', 'No'),
+#         )
+#
+#     def queryset(self, request, queryset):
+#         value = self.value()
+#         if value == 'Yes':
+#             return queryset.filter(member__is_active=True)
+#         elif value == 'No':
+#             return queryset.exclude(member__is_active=True)
+#         return queryset
 
 
 class IsAcceptedFilter(admin.SimpleListFilter):
@@ -157,110 +199,195 @@ class CustomUserAdmin(admin.ModelAdmin,):
     list_display = ['username', 'email', 'first_name', 'is_active']
 
 
+class ParticipantInline(ExportActionMixin, admin.ModelAdmin, ):
+    resource_class = ParticipantResource
+    # readonly_fields = ['document', 'gender', 'grade']
+    # ,'document','member__gender', 'member__grade', 'member__school', 'member__city', 'get_name','get_team',
+    list_display = ['member', 'get_name', 'get_team','get_grade',
+                    'get_gender','get_doc','get_school', 'get_city','selection_doc', 'get_phone',
+                    'par_is_paid', 'par_is_accepted','par_is_participated', 'par_accepting','par_finalize',]
+    # fields = ['member','document','gender', 'grade', 'ent_answer', 'school', 'city', 'get_name','get_team', 'is_accepted', 'is_paid', 'is_email_verified']
+    # list_filter = ('IsPaidFilter')
+
+    # inlines = [CustomUserAdmin]
+
+    def get_name(self, obj):
+        try:
+            return obj.member.first_name
+        except:
+            return False
+
+    def get_name(self, obj):
+        try:
+            return obj.member.first_name
+        except:
+            return False
+
+    def get_city(self, obj):
+        try:
+            return obj.member.city
+        except:
+            return False
+
+    def get_school(self, obj):
+        try:
+            return obj.member.school
+        except:
+            return False
+
+    def get_doc(self, obj):
+        # try:
+        link = "<a href={}>{}</a>".format(
+                    '127.0.0.1:8000/api/media/' + str(obj.member.document),
+        obj.member.first_name)
+        return mark_safe(link)
+        # except:
+        #     return False
+
+    def get_gender(self, obj):
+        try:
+            return obj.member.gender
+        except:
+            return False
+
+    def get_phone(self, obj):
+        try:
+            return obj.member.phone_number
+        except:
+            return False
+
+    def get_grade(self, obj):
+        try:
+            return obj.member.grade
+        except:
+            return False
+
+    def par_is_paid(self, obj):
+        try:
+            return obj.is_paid
+        except:
+            return False
+
+    def par_is_accepted(self, obj):
+        try:
+            return obj.is_accepted
+        except:
+            return False
+
+    def par_is_participated(self, obj):
+        try:
+            return obj.is_participated
+        except:
+            return False
+
+    def get_team(self, obj):
+        try:
+            if(obj.event_team):
+                display_text = str(obj.event_team.id) + '_' + obj.event_team.group_name + " (" + ", ".join([
+                    "<a href={}>{}</a>".format(
+                        reverse('admin:{}_{}_change'.format(Participant._meta.app_label, Participant._meta.model_name),
+                                args=(par.pk,)),
+                        par.member.username)
+                    for par in obj.event_team.team_participants.all()
+                ]) + ")"
+                if display_text:
+                    return mark_safe(display_text)
+            return "-"
+        except:
+            return ''
+
+    def par_accepting(self, obj):
+        try:
+            if obj.is_accepted:
+                return mark_safe('<a class="button" href="' + reverse('admin:unaccept_member',
+                                                                      args=[obj.pk]) + '">رد کن</a>' + '&nbsp;')
+            else:
+                return mark_safe('<a class="button" href="' + reverse('admin:accept_member',
+                                                                      args=[obj.pk]) + '">قبول کن</a>' + '&nbsp;')
+        except:
+            return None
+
+    def par_finalize(self, obj):
+        try:
+            if obj.is_participated:
+                return mark_safe('<a class="button" href="' + reverse('admin:unfinalize_member',
+                                                                      args=[obj.pk]) + '">نهایی کن</a>' + '&nbsp;')
+
+            else:
+                return mark_safe('<a class="button" href="' + reverse('admin:finalize_member',
+                                                                      args=[obj.pk]) + '">حذف کن</a>' + '&nbsp;')
+        except:
+            return None
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                'accept/<int:pk>/',
+                self.accept_member,
+                name='accept_member',
+            ),
+            path(
+                'unaccept/<int:pk>/',
+                self.unaccept_member,
+                name='unaccept_member',
+            ),
+            path(
+                'unfinalize/<int:pk>/',
+                self.unfinalize_member,
+                name='unfinalize_member',
+            ),
+            path(
+                'finalize/<int:pk>/',
+                self.finalize_member,
+                name='finalize_member',
+            )
+        ]
+        return custom_urls + urls
+
+    def accept_member(self, request, *args, **kwargs):
+        participant = Participant.objects.get(pk=kwargs['pk'])
+        participant.is_accepted = True
+        participant.save()
+        return redirect('/api/admin/accounts/participant')
+
+    def unaccept_member(self, request, *args, **kwargs):
+        participant = Participant.objects.get(pk=kwargs['pk'])
+        participant.is_accepted = False
+        participant.save()
+        return redirect('/api/admin/accounts/participant')
+
+    def finalize_member(self, request, *args, **kwargs):
+        participant = Participant.objects.get(pk=kwargs['pk'])
+        participant.is_participated = True
+        participant.save()
+        return redirect('/api/admin/accounts/participant')
+
+    def unfinalize_member(self, request, *args, **kwargs):
+        participant = Participant.objects.get(pk=kwargs['pk'])
+        participant.is_participated = False
+        participant.save()
+        return redirect('/api/admin/accounts/participant')
+
+    get_name.short_description = 'نام'
+    par_is_accepted.boolean = True
+    par_is_paid.boolean = True
+    par_is_participated.boolean = True
+    par_is_paid.short_description = 'تایید پرداخت'
+    par_is_accepted.short_description = 'پذیرش'
+    get_team.short_description = 'تیم'
+    par_is_participated.short_description = 'نهایی شده؟'
+    par_accepting.short_description = 'گزینش'
+    par_finalize.short_description = 'نهایی کردن'
+    get_doc.short_description = 'مدرک احراز هویت'
+    get_city.short_description = 'شهر'
+    get_school.short_description = 'مدرسه'
+    get_gender.short_description = 'دختر/پسر'
+    get_phone.short_description = 'شماره تماس'
+    get_grade.short_description = 'پایه'
 
 
-# class ParticipantInline(ExportActionMixin, admin.ModelAdmin, ):
-#     resource_class = ParticipantResource
-#     readonly_fields = ['document', 'gender', 'grade']
-#     list_display = ['member','document','gender', 'grade', 'ent_answer', 'school', 'city', 'get_name','get_team','account_actions', 'is_accepted', 'is_paid', 'is_email_verified']
-#     # fields = ['member','document','gender', 'grade', 'ent_answer', 'school', 'city', 'get_name','get_team', 'is_accepted', 'is_paid', 'is_email_verified']
-#     list_filter = ("gender", IsEmailVerifiedFilter, IsAcceptedFilter, IsPaidFilter)
-#     # inlines = [CustomUserAdmin]
-#
-#     def get_name(self, obj):
-#         try:
-#             return obj.member.first_name
-#         except:
-#             return False
-#
-#     def is_email_verified(self, obj):
-#         try:
-#             return obj.member.is_active
-#         except:
-#             return False
-#
-#     def is_paid(self, obj):
-#         try:
-#             return obj.is_activated
-#         except:
-#             return False
-#
-#     def is_accepted(self, obj):
-#         try:
-#             return obj.accepted
-#         except:
-#             return False
-#
-#     def get_team(self, obj):
-#         try:
-#             if(obj.team):
-#                 display_text = str(obj.team.id) + obj.team.group_name + " (" + ", ".join([
-#                     "<a href={}>{}</a>".format(
-#                         reverse('admin:{}_{}_change'.format(Participant._meta.app_label, Participant._meta.model_name),
-#                                 args=(member.pk,)),
-#                         member.member.email)
-#                     for member in obj.team.participant_set.all()
-#                 ]) + ")"
-#                 if display_text:
-#                     return mark_safe(display_text)
-#                 return "-"
-#         except:
-#             return ''
-#
-#
-#
-#     def account_actions(self, obj):
-#         try:
-#             if obj.accepted:
-#                 return mark_safe('<a class="button" href="' + reverse('admin:unaccept_member',
-#                                                                       args=[obj.pk]) + '">رد کن</a>' + '&nbsp;')
-#
-#             else:
-#                 return mark_safe('<a class="button" href="' + reverse('admin:accept_member',
-#                                                                       args=[obj.pk]) + '">قبول کن</a>' + '&nbsp;')
-#         except:
-#             return None
-#
-#     def get_urls(self):
-#         urls = super().get_urls()
-#         custom_urls = [
-#             path(
-#                 'accept/<int:pk>/',
-#                 self.accept_member,
-#                 name='accept_member',
-#             ),
-#             path(
-#                 'unaccept/<int:pk>/',
-#                 self.unaccept_member,
-#                 name='unaccept_member',
-#             )
-#         ]
-#         return custom_urls + urls
-#
-#     def accept_member(self, request, *args, **kwargs):
-#         participant = Participant.objects.get(pk=kwargs['pk'])
-#         participant.accepted = True
-#         participant.save()
-#         return redirect('/api/admin/accounts/participant')
-#
-#     def unaccept_member(self, request, *args, **kwargs):
-#         participant = Participant.objects.get(pk=kwargs['pk'])
-#         participant.accepted = False
-#         participant.save()
-#         return redirect('/api/admin/accounts/participant')
-#
-#     get_name.short_description = 'name'
-#     is_email_verified.short_description = 'تایید ایمیل'
-#     is_paid.short_description = 'تایید پرداخت'
-#     is_accepted.short_description = 'accepted'
-#     get_team.short_description = 'team'
-#     is_accepted.boolean = True
-#     is_email_verified.boolean = True
-#     is_paid.boolean = True
-#     account_actions.short_description = 'ACCEPT'
-#
-#
-#     list_per_page = sys.maxsize
+    list_per_page = sys.maxsize
 
 
 class TeamAdmin(admin.ModelAdmin):
@@ -352,7 +479,7 @@ class PaymentAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Member, CustomUserAdmin)
-admin.site.register(Participant)
+admin.site.register(Participant, ParticipantInline)
 admin.site.register(Team, TeamAdmin)
 admin.site.register(Mentor)
 admin.site.register(Player)
