cb8e45ff43b8fb8276016f3a62e34d8684019956
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-03-19 12:10:00 +0330

    access data

---
+++ accounts/management/commands/register_event_participants_in_workshop.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from accounts.models import Participant, Member, Team
+import os
+import logging
+from backup_data.problem_day_users import problem_day_users
+from fsm.models import FSM, PlayerWorkshop
+
+logger = logging.getLogger(__file__)
+
+
+class Command(BaseCommand):
+    help = 'Create user by phone_number list'
+
+    def add_arguments(self, parser):
+        parser.add_argument('workshop_name', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        print("----------")
+        workshop_name = options['workshop_name']
+        workshop = FSM.objects.get(name=workshop_name)
+        # test_workshop = FSM.objects.get(name="آموزش کار در پلتفرم")
+        teams = Team.objects.filter(event__name="مسافر صفر")
+        for t in teams:
+            if PlayerWorkshop.objects.filter(player=t) <= 0:
+                PlayerWorkshop.objects.create(player=t, workshop=workshop,
+                                                            current_state=workshop.first_state)
+                # player_workshop = PlayerWorkshop.objects.create(player=participant, workshop=test_workshop,
+                #                                             current_state=workshop.first_state)
+            # team_workshop = PlayerWorkshop.objects.create(player=t, workshop=workshop,
+            #                                             current_state=workshop.first_state)
+            # team_workshop2 = PlayerWorkshop.objects.create(player=t, workshop=test_workshop,
+            #                                             current_state=workshop.first_state)
+
