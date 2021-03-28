7fbe282462163df38fdc6c16cfd5815a04067cf0
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-03-28 14:57:47 +0430

    fixed minor bugs in go forward

---
+++ accounts/management/commands/create_participant.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from accounts.models import Participant, Member, Event, Team
+import os
+import logging
+from .users import users
+
+logger = logging.getLogger(__file__)
+
+
+class Command(BaseCommand):
+    help = 'Create users'
+
+    def handle(self, *args, **options):
+        for i in range(2, 20):
+            username = f'user_{i}'
+            password = '123456'
+
+            current_event = Event.objects.get(name='مسافر صفر')
+
+            team_code = Member.objects.make_random_password(length=6)
+
+            member = Member.objects.create(
+                username=username,
+                is_active=True,
+            )
+            member.set_password(password)
+
+            participant = Participant.objects.create(
+                member=member,
+                event=current_event,
+                player_type='PARTICIPANT',
+            )
+
+            team = Team.objects.create(team_code=team_code, event=current_event, player_type='TEAM', )
+            participant.event_team = team
+
+            member.save()
+            participant.save()
+
+            self.stdout.write(
+                self.style.SUCCESS('Successfully created user %s with password %s' % (username, password))
+            )
