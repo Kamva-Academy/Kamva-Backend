f36e5ecd55816a0f2b6b744caa6f48f4971c15c3
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-03-30 20:10:45 +0430

    get_team

--- accounts/management/commands/fix_scores.py
+++ accounts/management/commands/fix_scores.py
@@ -3,7 +3,7 @@ from accounts.models import Participant, Member, Team
 import os
 import logging
 
-from fsm.models import PlayerWorkshop, FSM, FSMEdge
+from fsm.models import PlayerWorkshop, FSM, ProblemBigAnswer
 from fsm.views.functions import get_scores_sum
 from scoring.models import ScoreTransaction
 from .users import users
@@ -15,12 +15,24 @@ class Command(BaseCommand):
     help = 'Get scores of a workshop'
 
     def handle(self, *args, **options):
-        for player_workshop in PlayerWorkshop.objects.filter(workshop__id=22):
-            for score_tr in ScoreTransaction.objects.filter(player_workshop=player_workshop,):
-                if 0 < score_tr.score:
-                    score_tr.is_valid = True
-                    score_tr.save()
+        for p in ProblemBigAnswer.objects.filter(state__fsm__id=22):
+            for player_workshop in PlayerWorkshop.objects.filter(workshop__id=22):
+                max_tr = None
+                for score_tr in ScoreTransaction.objects.filter(player_workshop=player_workshop,
+                                                                submitted_answer__player=player_workshop.player,
+                                                                submitted_answer__problem=p):
+                    if max_tr is None:
+                        max_tr = score_tr
+                    else:
+                        if max_tr.score < score_tr.score:
+                            max_tr = score_tr
 
+                    score_tr.is_valid = False
+                    score_tr.save()
+                max_tr.is_valid = True
+                print(f'for player_workshop {player_workshop.id} in problem {p.id} score{max_tr.score} is set true')
+                max_tr.save()
+                #
 
             # team = Team.objects.get(player_ptr_id=player_workshop.player.id)
             # print(f'{team.group_name}: {get_scores_sum(player_workshop)}')
