f3ef56b0c4c1185bb79bcf6039249b9955831b2f
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-03-30 19:37:08 +0430

    get_team

--- accounts/management/commands/fix_scores.py
+++ accounts/management/commands/fix_scores.py
@@ -3,7 +3,7 @@ from accounts.models import Participant, Member, Team
 import os
 import logging
 
-from fsm.models import PlayerWorkshop, FSM
+from fsm.models import PlayerWorkshop, FSM, FSMEdge
 from fsm.views.functions import get_scores_sum
 from scoring.models import ScoreTransaction
 from .users import users
@@ -16,13 +16,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for player_workshop in PlayerWorkshop.objects.filter(workshop__id=22):
-            for score_tr in ScoreTransaction.objects.filter(player_workshop=player_workshop,
-                                                            submitted_answer__player=player_workshop.player):
-                if 0 < score_tr.score <= 100:
-                    score = 6 * score_tr.score
-                    print(f'{score_tr.score} -> {score} : {score_tr.submitted_answer.problem.id}')
+            for score_tr in ScoreTransaction.objects.filter(player_workshop=player_workshop,):
+                if -100 <= score_tr.score < 0:
+                    score = 0.1 * score_tr.score
+                    print(f'{score_tr.score} -> {score}')
                     score_tr.score = score
                     score_tr.save()
 
+        for edge in FSMEdge.objects.all():
+            if edge.cost is not None and edge.cost > 0:
+                cost = edge.cost * 0.1
+                edge.cost = cost
+                edge.save()
             # team = Team.objects.get(player_ptr_id=player_workshop.player.id)
             # print(f'{team.group_name}: {get_scores_sum(player_workshop)}')
