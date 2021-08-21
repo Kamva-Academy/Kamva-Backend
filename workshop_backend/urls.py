42a3fc8e7d6d77968c32e43fc43131813b650026
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-08-21 21:29:14 +0430

    disabled api reference in production

--- workshop_backend/urls.py
+++ workshop_backend/urls.py
@@ -4,21 +4,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Rasta Workshop API",
-      default_version='v2',
-      # description="Test description",
+    openapi.Info(
+        title="Rasta Workshop API",
+        default_version='v2',
+        # description="Test description",
 
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -37,10 +35,12 @@ urlpatterns = [
     path('api/scoring/', include('scoring.urls')),
 
     # path('api(?P<format>\.json|\.yaml)/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
