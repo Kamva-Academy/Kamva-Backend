e25f82fb380164c14eaf42a56c7c85144b584989
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-08-22 06:31:11 +0430

    added permission for event/fsm modifiers to access profile

--- accounts/views/profile_view.py
+++ accounts/views/profile_view.py
@@ -19,7 +19,7 @@ class ProfileViewSet(ModelViewSet):
         user = self.request.user
         if isinstance(user, AnonymousUser):
             return User.objects.none()
-        elif user.is_staff or user.is_superuser:
+        elif user.is_staff or user.is_superuser or len(user.institutes) > 0 or len(user.events) > 0 or len(user.fsms) > 0:
             return User.objects.all()
         else:
             return User.objects.filter(id=user.id)
