da3df67e55fb7e78f8dd440a6c6d6a64cfe09459
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-08-30 21:59:53 +0430

    make team head

--- accounts/management/commands/create_players.py
+++ accounts/management/commands/create_players.py
@@ -15,9 +15,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for f in Event.objects.get(id=1).fsms.filter(is_active=True):
-            for r in RegistrationReceipt.objects.filter(is_participating=True):
-                if len(Player.objects.filter(user=r.user, fsm=f, receipt=r)) <= 0:
-                    p = Player.objects.create(user=r.user, fsm=f, receipt=r, current_state=f.first_state, last_visit=timezone.now())
-                    PlayerHistory.objects.create(player=p, state=f.first_state, start_time=p.last_visit)
+            if f.first_state:
+                for r in RegistrationReceipt.objects.filter(is_participating=True):
+                    if len(Player.objects.filter(user=r.user, fsm=f, receipt=r)) <= 0:
+                        p = Player.objects.create(user=r.user, fsm=f, receipt=r, current_state=f.first_state, last_visit=timezone.now())
+                        PlayerHistory.objects.create(player=p, state=f.first_state, start_time=p.last_visit)
 
-                    self.stdout.write(self.style.SUCCESS(f'created_for {r.user.username} & {f.name}'))
+                        self.stdout.write(self.style.SUCCESS(f'created_for {r.user.username} & {f.name}'))
