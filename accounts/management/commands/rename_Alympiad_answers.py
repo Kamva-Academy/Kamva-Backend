7805dfbe7d4c3e8da274146808ca9c439f430dfb
farhadi.erfan76 <farhadi.erfan76@gmail.com>
2021-11-27 21:20:10 +0330

    rename Alympiad answers

--- accounts/management/commands/rename_Alympiad_answers.py
+++ accounts/management/commands/rename_Alympiad_answers.py
@@ -1,6 +1,7 @@
 import os
 
 from django.conf import settings
+from django.core.exceptions import SuspiciousFileOperation
 from django.core.management import BaseCommand
 
 from fsm.models import RegistrationForm, Problem, UploadFileProblem, Team
@@ -15,13 +16,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         problem_id = options['problem'][0]
         upload_problem = UploadFileProblem.objects.get(id=problem_id)
+        i = 0
         for a in upload_problem.answers.filter(is_final_answer=True):
-            team = Team.objects.get_team_from_widget(a.submitted_by, upload_problem)
-            answer_file = a.answer_file
-            suffix = answer_file.name[answer_file.name.rfind('.'):]
-            old = answer_file.path
-            answer_file.name = f'/answers/{problem_id}-{team.team_head.id}{suffix}'
-            new = settings.MEDIA_ROOT + answer_file.name
-            os.rename(old, new)
-            a.save()
+            try:
+                team = Team.objects.get_team_from_widget(a.submitted_by, upload_problem)
+                answer_file = a.answer_file
+                suffix = answer_file.name[answer_file.name.rfind('.'):]
+                old = answer_file.path
+                answer_file.name = f'/answers/{problem_id}-{team.team_head.id}{suffix}'
+                new = settings.MEDIA_ROOT + answer_file.name
+                os.rename(old, new)
+                a.save()
+            except SuspiciousFileOperation:
+                i += 1
+                print('oh')
+                if i > 10:
+                    break
+                continue
             self.stdout.write(self.style.SUCCESS(f'Successfully rename to {answer_file.name}'))
