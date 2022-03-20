89ceaa13d04419f06687e279bc69c903912cb7a4
Arefeh <arefehahmadi902@gmail.com>
2022-03-20 00:59:06 -0700

    add event_metadata app

---
+++ event_metadata/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from accounts.models import *
+from fsm.models import RegistrationForm
+from PIL import Image
+
+# class StaffTitel(models.Model):
+#     name = models.CharField(max_length=64)
+
+#     def __str__(self) -> str:
+#         return self.name
+
+class StaffTeam(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+def get_staff_info_upload_path(instance, filename):
+    return f'registration_form/{instance.registration_form.id}/{filename}'
+
+class StaffInfo(models.Model):
+    account = models.ForeignKey('accounts.User', on_delete=models.PROTECT, related_name='staff_infos')
+    registration_form = models.ForeignKey(RegistrationForm, on_delete=models.CASCADE, related_name='staff_infos')
+    title = models.CharField(max_length=50, null=True, blank=True)
+    team = models.ManyToManyField(StaffTeam, blank=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to=get_staff_info_upload_path, null=True, blank=True)
+
+    class Meta:
+        unique_together = ['account', 'registration_form']
+
+    def save(self) -> None:
+        super().save()
+        if bool(self.image):
+            image = Image.open(self.image.path)
+            image.save(self.image.path,quality=75,optimize=True)
+
+    def __str__(self) -> str:
+        return f'{self.account}: {self.title} in {self.team}'
+
---
+++ event_metadata/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from event_metadata.models import StaffInfo, StaffTeam
+
+class StaffTeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StaffTeam
+        fields = ['name']
+
+class StaffInfoSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField(read_only=True)
+    team = StaffTeamSerializer(many=True)
+
+    class Meta:
+        model = StaffInfo
+        fields = ['account', 'registration_form', 'title', 'description', 'team', 'image_url']
+
+    def get_image_url(self, staff_info):
+        request = self.context.get('request')
+        if bool(staff_info.image):
+            image_url = staff_info.image.url
+            return request.build_absolute_uri(image_url)
+        return None
