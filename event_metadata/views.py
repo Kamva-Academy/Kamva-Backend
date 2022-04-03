9127db05c4aecd783f9e74540532e00be3ca7a40
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-04-03 17:49:57 +0430

    fix: remove permission from get_staff endpoint

--- event_metadata/views.py
+++ event_metadata/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from event_metadata.serializers import StaffInfoSerializer
 from event_metadata.models import StaffInfo
+from rest_framework.permissions import AllowAny
+
 
 class StaffInfoRetrieveView(generics.ListAPIView):
+    permission_classes = [AllowAny]
     serializer_class = StaffInfoSerializer
 
     def get_queryset(self):
