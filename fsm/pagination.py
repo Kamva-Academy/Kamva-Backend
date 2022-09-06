3e6cb4013d4af9d91bcb6138670ce5c264092fbe
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-09-06 18:59:05 +0430

    add pagination to TeamView

---
+++ fsm/pagination.py
@@ -0,0 +1,11 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 1000
+    max_page_size = 10000
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    max_page_size = 1000
