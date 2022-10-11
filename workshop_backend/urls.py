2006c0ee293700a3435c7b5fc3e296d08f2608b9
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2022-10-12 02:33:26 +0330

    improve static file resolver

--- workshop_backend/urls.py
+++ workshop_backend/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
 urlpatterns += [path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                 path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
