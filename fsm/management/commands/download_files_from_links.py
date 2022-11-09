645075550b403495d9e68bd4dda85bd5bb9809fc
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-11-10 03:14:43 +0330

    add a command to download files from links

---
+++ fsm/management/commands/download_files_from_links.py
@@ -0,0 +1,37 @@
+from fsm.utils import get_django_file
+from fsm.models import Image, Video
+
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'download files from links'
+
+    def handle(self, *args, **options):
+        images = Image.objects.all()
+        videos = Video.objects.all()
+        all_count = len(images) + len(videos)
+        done_count = 0
+        for image in images:
+            try:
+                if not image.file:
+                    link_file = get_django_file(image.link)
+                    image.file = link_file
+                    image.save()
+            except:
+                pass
+            finally:
+                done_count += 1
+                print(f'{done_count}/{all_count}')
+
+        for video in videos:
+            try:
+                if not video.file:
+                    link_file = get_django_file(video.link)
+                    video.file = link_file
+                    video.save()
+            except:
+                pass
+            finally:
+                done_count += 1
+                print(f'{done_count}/{all_count}')
