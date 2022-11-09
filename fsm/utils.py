2f9f6aff5e3698fafdb9e5cd9e513d0d246f3940
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-11-09 18:06:25 +0330

    fix file size

--- fsm/utils.py
+++ fsm/utils.py
@@ -7,11 +7,15 @@ import os
 def get_django_file(url: str):
     r = requests.get(url, allow_redirects=True)
 
+    print(r.content)
+
     file_name = url.rsplit('/', 1)[1]
     file_type = r.headers.get('content-type')
+    file_size = int(r.headers.get('content-length'))
 
     file_io = BytesIO(r.content)
+
     django_file = InMemoryUploadedFile(
-        file_io, None, file_name, file_type, file_io.tell(), None)
+        file_io, None, file_name, file_type,  file_size, None)
 
     return django_file
