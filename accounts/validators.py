a086fa3ca478ad7dc2ede8a61b371154694ce72c
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-12-14 15:32:25 +0330

    disable phone number length check

--- accounts/validators.py
+++ accounts/validators.py
@@ -6,8 +6,8 @@ from errors.error_codes import serialize_error
 def phone_number_validator(phone_number):
     if not phone_number.isdigit():
         raise serializers.ValidationError(serialize_error('4000'))
-    elif len(phone_number) < 10:
-        raise serializers.ValidationError(serialize_error('4001'))
+    # elif len(phone_number) < 10:
+    #     raise serializers.ValidationError(serialize_error('4001'))
     return phone_number
 
 
