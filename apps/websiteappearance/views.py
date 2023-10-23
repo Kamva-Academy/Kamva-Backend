81fbde73cca1a11d457f478e35a2448d5b796028
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-10-23 18:42:43 +0330

    remove redundant imports

--- apps/websiteappearance/views.py
+++ apps/websiteappearance/views.py
@@ -1,10 +1,6 @@
-from rest_framework import viewsets
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework import viewsets
 from rest_framework.response import Response
-from apps.fsm.models import *
 
 from apps.websiteappearance.models import Banner
 from apps.websiteappearance.serializers import BannerSerializer
