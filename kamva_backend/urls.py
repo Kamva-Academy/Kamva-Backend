8b04b8b50506c611a1550d322cf11a19aa557e7b
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-12-29 15:12:11 +0330

    add urls of reporting api

--- kamva_backend/urls.py
+++ kamva_backend/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
 
 urlpatterns += [path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                 path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-                path('report/' , include('report.urls')),]
+                path('report/', include('apps.report.urls')), ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
