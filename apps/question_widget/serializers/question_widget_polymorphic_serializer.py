27b4a93e5543743eb2a06951871b4163cb2e0d1a
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-01-02 14:24:48 +0330

    delete question_widget models

--- apps/question_widget/serializers/question_widget_polymorphic_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_polymorphic.serializers import PolymorphicSerializer
-from apps.question_widget.models import InviteeUsernameQuestion
-from apps.question_widget.serializers.invitee_username_question_serializer import InviteeUsernameQuestionSerializer
-
-
-class QuestionPolymorphicSerializer(PolymorphicSerializer):
-    model_serializer_mapping = {
-        InviteeUsernameQuestion: InviteeUsernameQuestionSerializer,
-    }
--- apps/question_widget/urls.py
+++ apps/question_widget/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from apps.question_widget.views.answer_view import UploadAnswerViewSet, AnswerViewSet
-
 router = DefaultRouter()
 
 urlpatterns = [
 ]
-
-router.register(r'upload_answer', UploadAnswerViewSet,
-                basename='upload_answer')
-router.register(r'answers', AnswerViewSet, basename='answers')
 
 urlpatterns += router.urls
